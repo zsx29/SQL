02e085abfa5cbf9bb004df49d4c95ca76fb1778c
zsx29 <zsx29@naver.com>
2021-07-28 13:17:35 +0900

    2021-07-28 update

---
+++ 1차 카테고리 데이터.sql
@@ -0,0 +1,17 @@
+# 날짜 : 2021-07-21
+# 이름 : 박재형
+# 내용 : 1차 카테고리 등록
+
+
+INSERT INTO `km_cate1` VALUES (1, '브랜드패션');
+INSERT INTO `km_cate1` VALUES (2, '패션의류/잡화/뷰티');
+INSERT INTO `km_cate1` VALUES (3, '유아동');
+INSERT INTO `km_cate1` VALUES (4, '식품/생필품');
+INSERT INTO `km_cate1` VALUES (5, '홈데코/문구/취미/반려');
+INSERT INTO `km_cate1` VALUES (6, '컴푸터/디지털가전');
+INSERT INTO `km_cate1` VALUES (7, '스포츠/건강/렌탈');
+INSERT INTO `km_cate1` VALUES (8, '자동차/공구');
+INSERT INTO `km_cate1` VALUES (9, '여행/도서/티켓/e쿠폰');
+
+
+SELECT * FROM `km_cate1`;
